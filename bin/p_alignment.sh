9290a342693b3901393d06d621daa6763fa6442e
HeribertoVaquezCardona <44846726+HeribertoVaquezCardona@users.noreply.github.com>
2019-11-22 18:53:09 -0600

    Update p_alignment.sh

--- bin/p_alignment.sh
+++ bin/p_alignment.sh
@@ -6,13 +6,17 @@ home="/LUSTRE/Genetica/hvazquez/"
 ngm="Cibiv-NextGenMap-33e92fb/bin/ngm-0.5.5/ngm"
 ref="../data/B73_v4/Zea_mays.B73_RefGen_v4.dna_rm.toplevel.fa.gz"
 bam="../out_bam"
-fastq="/LUSTRE/Genetica/hvazquez/out"
+fastq="../out"
+log="../out_log"
 
 
-ls  $fastq | grep fastq |  grep R | sed s/.R1.fastq.gz// |sed s/.R2.fastq.gz// | uniq > lele.txt
+ls  $fastq | grep fastq |  grep R | sed s/.R1.fastq.gz// |sed s/.R2.fastq.gz// | uniq > $home/meta/samplelist.txt
 
 while read name
 do
+	if [  ! -f $bam/$name.sort.bam ]; then
+
+{
 	echo "Aligning paired $name"
 
 $ngm -r $ref -p -1 ../out/${name}.R1.fastq.gz -2 ../out/${name}.R2.fastq.gz -o $bam/$name.bam  -t 8
@@ -24,5 +28,4 @@ samtools sort -@ 8 $bam/$name.bam > $bam/${name}.sort.bam
 	rm $bam/$name.bam
 }>& $log/$name.alignment.log
 fi
-
 done < $home/meta/samplelist.txt
