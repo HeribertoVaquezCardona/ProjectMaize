32ab89cfd4de429714437c47d8145892c34367c1
HeribertoVaquezCardona <44846726+HeribertoVaquezCardona@users.noreply.github.com>
2020-01-06 12:29:40 -0600

    Update merge_gvcf_1.sh

--- bin/merge_gvcf_1.sh
+++ bin/merge_gvcf_1.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#SBATCH -p keri
+#SBATCH -n 10
+#SBATCH --mem=60000
 
+combinedGVCFs="/LUSTRE/Genetica/hvazquez/gvcf_1/gvcf"
+tmp=""
+ref="/LUSTRE/Genetica/hvazquez/data/B73_v4/Zea_mays.B73_RefGen_v4.dna_rm.toplevel.fa"
+home="/LUSTRE/Genetica/hvazquez"
+
+ls $combinedGVCFs | grep "vcf" | grep -v ".idx"   > $home/meta/GVCFs_1.samplelist.txt
+
+while read prefix
+do
+        tmp="$tmp --variant $combinedGVCFs/$prefix"
+done < $home/meta/GVCFs_1.samplelist.txt
+
+./gatk-4.1.4.1/gatk --java-options "-Xmx60g" CombineGVCFs \
+        -R /LUSTRE/Genetica/hvazquez/data/B73_v4/Zea_mays.B73_RefGen_v4.dna_rm.toplevel.fa \
+        $tmp \
+        -O /LUSTRE/Genetica/hvazquez/gvcf_1/gvcf/Zea_mays.GPv4.vcf \
+        --read-filter MappingQualityReadFilter \
+        --read-filter NotDuplicateReadFilter
